5375594580ec0eb7a6cb5d4d6f984d0b05f83656
damouu <mouadsehbaoui@gmail.com>
2025-09-14 22:19:33 +0200

    Refactor #LAP-3 Done Unassigned Fixed Done
    refactor the UserFactory.php
    
    Reviewed-by: damouu <mouadsehbaoui@gmail.com>
    
    Tested-by: damouu <mouadsehbaoui@gmail.com>

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -2,12 +2,12 @@
 
 namespace Database\Factories;
 
+use App\Models\User;
 use Illuminate\Database\Eloquent\Factories\Factory;
 use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Str;
 
 /**
- * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\User>
+ * @extends Factory<User>
  */
 class UserFactory extends Factory
 {
@@ -25,20 +25,13 @@ class UserFactory extends Factory
     {
         return [
             'name' => fake()->name(),
+            'studentCardUUID' => fake()->unique()->uuid(),
             'email' => fake()->unique()->safeEmail(),
-            'email_verified_at' => now(),
             'password' => static::$password ??= Hash::make('password'),
-            'remember_token' => Str::random(10),
+            'avatar_img_URL' => fake()->unique()->url(),
+            'last_loggedIn_at' => fake()->dateTime(),
+            'updated_at' => fake()->dateTime(),
+            'deleted_at' => fake()->dateTime(),
         ];
-    }
-
-    /**
-     * Indicate that the model's email address should be unverified.
-     */
-    public function unverified(): static
-    {
-        return $this->state(fn (array $attributes) => [
-            'email_verified_at' => null,
-        ]);
     }
 }
